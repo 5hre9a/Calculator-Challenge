a05076a5d4505dbb955f075ac68e6089c138fcd4
SHREYA MALOGI <5hre9a@gmail.com>
2020-07-23 03:10:06 +0530

    4th commit with bmi

--- calculator.js
+++ calculator.js
@@ -20,6 +20,20 @@ app.post("/", function(req, res){
         res.send("the answer is = " + result);
 });
 
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmicalculator.html");
+  });
+  
+  app.post("/", function(req, res){
+  
+      var weight = parseFloat(req.body.weight);
+      var height = parseFloat(req.body.height);
+  
+      var bmi = weight / (height * weight);
+  
+          res.send("your BMI is = " + bmi);
+  });
+
 app.listen(3000, function(){
     console.log("server is running on port 3000");
 });
